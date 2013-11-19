dcae4251a65b6ac99dbb593ed22fe38bf4915182
Markus Roth <roth@videmo.de>
2013-11-19 13:44:54 +0100

    Fix description appending.

--- chiliproject2gitlab.py
+++ chiliproject2gitlab.py
@@ -121,10 +121,10 @@ for issue in chiliproject_issues:
 
     # description
     gitlab_issue['description'] = issue['Description']
-    gitlab_issue['description'] + '\n\n'
+    gitlab_issue['description'] += '\n\n'
     for k in ('#', 'Created', 'Due date', '% Done', 'Updated'):
         if issue[k]:
-            gitlab_issue['description'] + '%s: %s\n' % (k, issue[k])
+            gitlab_issue['description'] += '%s: %s\n' % (k, issue[k])
 
     if labels:
         gitlab_issue['labels'] = ','.join(labels)

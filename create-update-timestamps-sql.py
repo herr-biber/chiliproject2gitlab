5d842e0cf41f06f558960ce5e1998c959e48181c
Markus Roth <roth@videmo.de>
2013-11-25 18:35:32 +0100

    Update event timings and tagging timings.

--- create-update-timestamps-sql.py
+++ create-update-timestamps-sql.py
@@ -10,6 +10,23 @@ pattern_issues = 'UPDATE  `gitlab`.`issues` \
 SET `created_at` = \'%s\', `updated_at` = \'%s\' \
 WHERE  `issues`.`id` = %d;'
 
+pattern_events_create_issue = 'UPDATE  `gitlab`.`events` \
+SET `created_at` = \'%s\', `updated_at` = \'%s\' \
+WHERE `events`.`target_id` = %d \
+AND `target_type` = \'Issue\' \
+AND `action` = 1;'
+
+pattern_events_close_issue = 'UPDATE  `gitlab`.`events` \
+SET  `created_at` =  \'%s\', `updated_at` = \'%s\' \
+WHERE `events`.`target_id` = %d \
+AND `target_type` = \'Issue\' \
+AND `action` = 3;'
+
+pattern_taggings = 'UPDATE `gitlab`.`taggings` \
+SET `created_at` = \'%s\' \
+WHERE `taggable_id` = %d \
+AND `taggable_type` = \'Issue\';'
+
 with open('new_issue_timestamps.json') as fd:
     ids = json.load(fd)
 
@@ -18,3 +35,7 @@ for id, timestamps in ids.items():
 
 for id, timestamps in ids.items():
     print(pattern_notes % (timestamps['updated'], timestamps['updated'], int(id)))
+
+for id, timestamps in ids.items():
+    print(pattern_events_create_issue % (timestamps['created'], timestamps['created'], int(id)))
+    print(pattern_events_close_issue  % (timestamps['updated'], timestamps['updated'], int(id)))
